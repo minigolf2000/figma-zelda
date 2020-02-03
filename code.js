fd1e38b0776e709904723532a9ebf0c63a79dffb
Golf Sinteppadon <golf.sinteppadon@gmail.com>
2020-02-02 22:03:23 -0800

    basic collision detection

--- code.js
+++ code.js
@@ -4,76 +4,149 @@
 // You can access browser APIs in the <script> tag inside "ui.html" which has a
 // full browser enviroment (see documentation).
 // This shows the HTML page in "ui.html".
-const FPS = 60;
-function nextFrame() {
-}
-function init() {
-    figma.showUI(__html__);
+let linkNode = null;
+let worldNode;
+let sprites = null;
+let walls = {};
+function main() {
+    if (!figma.currentPage.selection) {
+        figma.closePlugin("Please have Link selected while running the Plugin");
+        return false;
+    }
     if (figma.currentPage.selection[0].type !== 'INSTANCE') {
         figma.closePlugin("Selected node must be an instance, whose master lives in 'sprites' frame");
-        return;
+        return false;
     }
-    const link = figma.currentPage.selection[0];
-    const sprites = loadSprites(link);
+    linkNode = figma.currentPage.selection[0];
+    sprites = loadSprites(linkNode);
     if (!sprites) {
         figma.closePlugin("Error loading sprites");
+        return false;
+    }
+    if (linkNode.parent.type !== 'FRAME') {
+        figma.closePlugin("World must be a frame");
         return;
     }
-    console.log(sprites);
-    figma.ui.onmessage = msg => {
-        if (msg.type === 'keydown') {
-            switch (msg.keyCode) {
-                case 13: // ENTER
-                case 16: // SHIFT
-                case 17: // CTRL
-                case 18: // ALT
-                case 32: // SPACE
-                    action(link, sprites);
-                    break;
-                case 37: // LEFT_ARROW
-                case 65: // A
-                    moveLeft(link, sprites);
-                    break;
-                case 38: // UP_ARROW
-                case 87: // W
-                    moveUp(link, sprites);
-                    break;
-                case 39: // RIGHT_ARROW
-                case 68: // D
-                    moveRight(link, sprites);
-                    break;
-                case 40: // DOWN_ARROW
-                case 83: // S
-                    moveDown(link, sprites);
-                    break;
-            }
-        }
-        // Call this when Link dies
-        // figma.closePlugin();
-    };
+    worldNode = linkNode.parent;
+    walls = loadWalls();
+    console.log(walls);
+    return true;
 }
-function action(link, sprites) {
+figma.showUI(__html__);
+// let health = event.data.pluginMessage.health
+// let newHealth = ''
+// while (health >= 1) {
+//   newHealth += '💗'
+//   health -= 1
+// }
+// while (event.data.pluginMessage.health >= .5) {
+//   newHealth += '💔'
+// }
+// newevent.data.pluginMessage.health
+//  event.data.pluginMessage.health.re
+figma.ui.postMessage({ health: '💗💗💗' });
+const FPS = 60;
+const keysPressed = {
+    up: false, down: false, left: false, right: false, action: false
+};
+const linkState = {};
+figma.ui.onmessage = msg => {
+    switch (msg.keyCode) {
+        case 13: // ENTER
+        case 16: // SHIFT
+        case 17: // CTRL
+        case 18: // ALT
+        case 32: // SPACE
+            keysPressed.action = (msg.type === 'keydown') ? true : false;
+            break;
+        case 37: // LEFT_ARROW
+        case 65: // A
+            keysPressed.left = (msg.type === 'keydown') ? true : false;
+            break;
+        case 38: // UP_ARROW
+        case 87: // W
+            keysPressed.up = (msg.type === 'keydown') ? true : false;
+            break;
+        case 39: // RIGHT_ARROW
+        case 68: // D
+            keysPressed.right = (msg.type === 'keydown') ? true : false;
+            break;
+        case 40: // DOWN_ARROW
+        case 83: // S
+            keysPressed.down = (msg.type === 'keydown') ? true : false;
+            break;
+    }
+    // Call this when Link dies
+    // figma.closePlugin();
+};
+function nextFrame() {
+    if (keysPressed.action) {
+        action();
+    }
+    if (keysPressed.left) {
+        moveLeft();
+    }
+    if (keysPressed.up) {
+        moveUp();
+    }
+    if (keysPressed.right) {
+        moveRight();
+    }
+    if (keysPressed.down) {
+        moveDown();
+    }
+}
+function action() {
     console.log("action");
 }
-function moveLeft(link, sprites) {
+function moveLeft() {
     console.log("moveLeft");
-    link.masterComponent = sprites['side'][0];
-    link.x -= 2;
+    linkNode.masterComponent = sprites['side'][0];
+    const newX = linkNode.x - 4;
+    const newY = linkNode.y;
+    if (isColliding(newX, newY)) {
+        return;
+    }
+    linkNode.x = newX;
+    linkNode.y = newY;
 }
-function moveUp(link, sprites) {
+function isColliding(x, y) {
+    var _a, _b, _c, _d;
+    if (x < 0 || y < 0 || x > worldNode.width || y > worldNode.height) {
+        return true;
+    }
+    return (((_a = walls[Math.floor(x / 16) * 16]) === null || _a === void 0 ? void 0 : _a[Math.floor(y / 16) * 16]) || ((_b = walls[Math.floor(x / 16) * 16]) === null || _b === void 0 ? void 0 : _b[Math.ceil(y / 16) * 16]) || ((_c = walls[Math.ceil(x / 16) * 16]) === null || _c === void 0 ? void 0 : _c[Math.floor(y / 16) * 16]) || ((_d = walls[Math.ceil(x / 16) * 16]) === null || _d === void 0 ? void 0 : _d[Math.ceil(y / 16) * 16]));
+}
+function moveUp() {
     console.log("moveUp");
-    link.masterComponent = sprites['up'][0];
-    link.y -= 2;
+    linkNode.masterComponent = sprites['up'][0];
+    const newX = linkNode.x;
+    const newY = linkNode.y - 4;
+    if (isColliding(newX, newY)) {
+        return;
+    }
+    linkNode.x = newX;
+    linkNode.y = newY;
 }
-function moveRight(link, sprites) {
-    link.masterComponent = sprites['side'][0];
-    console.log("moveRight");
-    link.x += 2;
+function moveRight() {
+    linkNode.masterComponent = sprites['side'][0];
+    const newX = linkNode.x + 4;
+    const newY = linkNode.y;
+    if (isColliding(newX, newY)) {
+        return;
+    }
+    linkNode.x = newX;
+    linkNode.y = newY;
 }
-function moveDown(link, sprites) {
-    link.masterComponent = sprites['down'][0];
-    console.log("moveDown");
-    link.y += 2;
+function moveDown() {
+    linkNode.masterComponent = sprites['down'][0];
+    const newX = linkNode.x;
+    const newY = linkNode.y + 4;
+    if (isColliding(newX, newY)) {
+        return;
+    }
+    linkNode.x = newX;
+    linkNode.y = newY;
 }
 function loadSprites(link) {
     const sprites = {};
@@ -99,5 +172,18 @@ function loadSprites(link) {
     ];
     return sprites;
 }
-init();
-setInterval(nextFrame, 1000 / FPS);
+function loadWalls() {
+    const walls = {};
+    worldNode.children.forEach((node) => {
+        if (node.name === 'tree' || node.name.includes('rock')) {
+            if (!walls[node.x]) {
+                walls[node.x] = {};
+            }
+            walls[node.x][node.y] = true;
+        }
+    });
+    return walls;
+}
+if (main()) {
+    setInterval(nextFrame, 1000 / FPS);
+}
--- code.ts
+++ code.ts
@@ -7,85 +7,184 @@
 
 // This shows the HTML page in "ui.html".
 
-const FPS = 60
-function nextFrame() {
+let linkNode: InstanceNode = null
+let worldNode: FrameNode
+let sprites: Sprites = null
+let walls = {}
 
-}
-
-function init() {
-  figma.showUI(__html__);
+function main() {
+  if (!figma.currentPage.selection) {
+    figma.closePlugin("Please have Link selected while running the Plugin")
+    return false
+  }
 
   if (figma.currentPage.selection[0].type !== 'INSTANCE') {
     figma.closePlugin("Selected node must be an instance, whose master lives in 'sprites' frame")
-    return 
+    return false
   }
 
-  const link: InstanceNode = figma.currentPage.selection[0]
-  const sprites = loadSprites(link)
+  linkNode = figma.currentPage.selection[0]
+  sprites = loadSprites(linkNode)
   if (!sprites) {
     figma.closePlugin("Error loading sprites")
+    return false
+  }
+  if (linkNode.parent.type !== 'FRAME') {
+    figma.closePlugin("World must be a frame")
     return
   }
-  console.log(sprites)
+  worldNode = linkNode.parent
 
-  figma.ui.onmessage = msg => {
-    if (msg.type === 'keydown') {
-      switch (msg.keyCode as number) {
-        case 13: // ENTER
-        case 16: // SHIFT
-        case 17: // CTRL
-        case 18: // ALT
-        case 32: // SPACE
-          action(link, sprites)
-          break
-        case 37: // LEFT_ARROW
-        case 65: // A
-          moveLeft(link, sprites)
-          break
-        case 38: // UP_ARROW
-        case 87: // W
-          moveUp(link, sprites)
-          break
-        case 39: // RIGHT_ARROW
-        case 68: // D
-          moveRight(link, sprites)
-          break
-        case 40: // DOWN_ARROW
-        case 83: // S
-          moveDown(link, sprites)
-          break
-      }
-    }
-
-    // Call this when Link dies
-    // figma.closePlugin();
-  };
-  
+  walls = loadWalls()
+  console.log(walls)
+  return true
 }
 
-function action(link: InstanceNode, sprites: {}) {
+figma.showUI(__html__);
+
+// let health = event.data.pluginMessage.health
+// let newHealth = ''
+// while (health >= 1) {
+//   newHealth += '💗'
+//   health -= 1
+// }
+// while (event.data.pluginMessage.health >= .5) {
+//   newHealth += '💔'
+// }
+
+// newevent.data.pluginMessage.health
+//  event.data.pluginMessage.health.re
+
+figma.ui.postMessage({health: '💗💗💗'})
+const FPS = 60
+interface Buttons {
+  up: boolean
+  down: boolean
+  left: boolean
+  right: boolean
+  action: boolean
+}
+const keysPressed: Buttons = {
+  up: false, down: false, left: false, right: false, action: false
+}
+
+interface State {
+  health: number
+  x: number
+  y: number
+  swordActiveFrame?: number
+}
+const linkState = {
+
+}
+figma.ui.onmessage = msg => {
+  switch (msg.keyCode as number) {
+    case 13: // ENTER
+    case 16: // SHIFT
+    case 17: // CTRL
+    case 18: // ALT
+    case 32: // SPACE
+      keysPressed.action = (msg.type === 'keydown') ? true : false
+      break
+    case 37: // LEFT_ARROW
+    case 65: // A
+      keysPressed.left = (msg.type === 'keydown') ? true : false
+      break
+    case 38: // UP_ARROW
+    case 87: // W
+      keysPressed.up = (msg.type === 'keydown') ? true : false
+      break
+    case 39: // RIGHT_ARROW
+    case 68: // D
+      keysPressed.right = (msg.type === 'keydown') ? true : false
+      break
+    case 40: // DOWN_ARROW
+    case 83: // S
+      keysPressed.down = (msg.type === 'keydown') ? true : false
+      break
+}
+
+// Call this when Link dies
+// figma.closePlugin();
+};
+
+function nextFrame() {
+  if (keysPressed.action) {
+    action()
+  }
+  if (keysPressed.left) {
+    moveLeft()
+  }
+  if (keysPressed.up) {
+    moveUp()
+  }
+  if (keysPressed.right) {
+    moveRight()
+  }
+  if (keysPressed.down) {
+    moveDown()
+  }
+}
+
+function action() {
   console.log("action")
 }
 
-function moveLeft(link: InstanceNode, sprites: {}) {
+function moveLeft() {
   console.log("moveLeft")
-  link.masterComponent = sprites['side'][0]
-  link.x -= 2
+  linkNode.masterComponent = sprites['side'][0]
+  const newX = linkNode.x - 4
+  const newY = linkNode.y
+
+  if (isColliding(newX, newY)) {
+    return
+  }
+  linkNode.x = newX; linkNode.y = newY
 }
-function moveUp(link: InstanceNode, sprites: {}) {
+
+function isColliding(x: number, y: number) {
+  if (x < 0 || y < 0 || x > worldNode.width || y > worldNode.height) {
+    return true
+  }
+  return (
+    walls[Math.floor(x / 16) * 16]?.[Math.floor(y / 16) * 16] ||
+    walls[Math.floor(x / 16) * 16]?.[Math.ceil(y / 16) * 16] ||
+    walls[Math.ceil(x / 16) * 16]?.[Math.floor(y / 16) * 16] ||
+    walls[Math.ceil(x / 16) * 16]?.[Math.ceil(y / 16) * 16]
+  )
+}
+function moveUp() {
   console.log("moveUp")
-  link.masterComponent = sprites['up'][0]
-  link.y -= 2
+  linkNode.masterComponent = sprites['up'][0]
+  const newX = linkNode.x
+  const newY = linkNode.y - 4
+
+  if (isColliding(newX, newY)) {
+    return
+  }
+  linkNode.x = newX; linkNode.y = newY
 }
-function moveRight(link: InstanceNode, sprites: {}) {
-  link.masterComponent = sprites['side'][0]
-  console.log("moveRight")
-  link.x += 2
+
+function moveRight() {
+  linkNode.masterComponent = sprites['side'][0]
+  const newX = linkNode.x + 4
+  const newY = linkNode.y
+
+  if (isColliding(newX, newY)) {
+    return
+  }
+  linkNode.x = newX; linkNode.y = newY
 }
-function moveDown(link: InstanceNode, sprites: {}) {
-  link.masterComponent = sprites['down'][0]
-  console.log("moveDown")
-  link.y += 2
+
+function moveDown() {
+  linkNode.masterComponent = sprites['down'][0]
+  const newX = linkNode.x
+  const newY = linkNode.y + 4
+
+  if (isColliding(newX, newY)) {
+    return
+  }
+  linkNode.x = newX; linkNode.y = newY
 }
 
 interface Sprites {
@@ -120,6 +219,17 @@ function loadSprites(link: InstanceNode) {
   return sprites
 }
 
+function loadWalls() {
+  const walls = {}
+  worldNode.children.forEach((node: SceneNode) => {
+    if (node.name === 'tree' || node.name.includes('rock')) {
+      if (!walls[node.x]) {walls[node.x] = {}}
+      walls[node.x][node.y] = true
+    }
+  })
 
-init()
-setInterval(nextFrame, 1000 / FPS)
+  return walls
+}
+if (main()) {
+  setInterval(nextFrame, 1000 / FPS)
+}
